e4c2226445ab66a1f6ec9c65454757ee29aa87a7
nasimudheen-t <comnasim756@gmail.com>
2025-10-23 12:05:16 +0530

    api changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),  // <-- add this
+    tailwindcss(),
   ],
+  server: {
+    proxy: {
+      // This will forward any request starting with /api to your backend
+      '/api': {
+        target: 'http://167.235.241.103:8000', // your backend URL
+        changeOrigin: true,
+        secure: false, // because backend is HTTP, not HTTPS
+        rewrite: (path) => path.replace(/^\/api/, ''), // optional, removes '/api' prefix
+      },
+    },
+  },
 })
